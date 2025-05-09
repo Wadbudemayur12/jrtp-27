993683d8b6b8f527c71416035cd089fd0055e927
wadbudemayur12 <mayurwadbude17@gmail.com>
2025-05-09 18:15:11 +0530

    task 320 completed

--- user.java
+++ user.java
@@ -3,8 +3,17 @@ public class user{
   public static void main(String[] args){
    System.out.println("user branch created");
   }
+<<<<<<< Updated upstream
   //task 321
   public void m2(){
      //some logic
   }
+=======
+  //TASK 320
+    public void m1(){
+      int i=10;
+      int j =20;
+      //logic
+    }
+>>>>>>> Stashed changes
 }
