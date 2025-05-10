3446b249c22dc8d41336ad10224fe8872e3bd1cd
wadbudemayur12 <mayurwadbude17@gmail.com>
2025-05-10 10:04:19 +0530

    Revert "Update user.java"
    This reverts commit a2b0fdeedf9375e05e5c9e8810705b52ccdd7fe7.

--- user.java
+++ user.java
@@ -3,21 +3,17 @@ public class user{
   public static void main(String[] args){
    System.out.println("user branch created");
   }
-
+<<<<<<< Updated upstream
   //task 321
   public void m2(){
      //some logic
   }
-
+=======
   //TASK 320
     public void m1(){
       int i=10;
       int j =20;
       //logic
     }
-
-  public void m4(){
-    System.out.println("we are in the m4 method");
-  }
-
+>>>>>>> Stashed changes
 }
