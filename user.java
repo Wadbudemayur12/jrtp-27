a2b0fdeedf9375e05e5c9e8810705b52ccdd7fe7
Wadbudemayur12 <165123926+Wadbudemayur12@users.noreply.github.com>
2025-05-10 10:00:29 +0530

    Update user.java

--- user.java
+++ user.java
@@ -3,17 +3,21 @@ public class user{
   public static void main(String[] args){
    System.out.println("user branch created");
   }
-<<<<<<< Updated upstream
+
   //task 321
   public void m2(){
      //some logic
   }
-=======
+
   //TASK 320
     public void m1(){
       int i=10;
       int j =20;
       //logic
     }
->>>>>>> Stashed changes
+
+  public void m4(){
+    System.out.println("we are in the m4 method");
+  }
+
 }
